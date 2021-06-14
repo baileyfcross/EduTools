d073697e422b6ab821df2d53662949ffe0478a72
Jim Teresco <terescoj@gmail.com>
2021-06-14 14:07:57 -0400

    Create file for Quadtree construction AV, #224

---
+++ HighwayDataExaminer/hdxquadtreeav.js
@@ -0,0 +1,28 @@
+//
+// HDX Quadtree construction AV
+//
+// METAL Project
+//
+// Primary Authors: Jim Teresco
+//
+
+var hdxQuadtreeAV = {
+
+    // entries for list of AVs
+    value: "quadtree",
+    name: "Quadtree Construction",
+    description: "Construct a quadtree by inserting all waypoints and refining as needed.",
+
+    code: "TBD",
+    
+    prepToStart() {
+    },
+    
+    nextStep() {
+
+    },
+
+    setupUI() {},
+
+    cleanupUI() {}
+};
