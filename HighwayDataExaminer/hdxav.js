3e3f8488c13f88829509ce30034be1906bbafc61
LukeJennings1729 <lg04jenn@siena.edu>
2021-06-15 13:08:25 -0400

    started bounding box functionality

--- HighwayDataExaminer/hdxav.js
+++ HighwayDataExaminer/hdxav.js
@@ -119,7 +119,7 @@ var hdxAV = {
         this.avList.push(hdxDFSRecAV);
         this.avList.push(hdxBFConvexHullAV);
         this.avList.push(hdxClosestPairsRecAV);
-        //this.avList.push(hdxQuadtreeAV);
+        this.avList.push(hdxQuadtreeAV);
         
         // populate the algorithm selection select with options
         // from the avList
