fd2e764ed1633c09373775b2476a3b5dd16f9de8
LukeJennings1729 <lg04jenn@siena.edu>
2021-06-26 05:56:51 -0400

    adding hdxorderingav.js

---
+++ HighwayDataExaminer/hdxorderingav.js
@@ -0,0 +1,27 @@
+//
+// HDX Traversal Orderings AV
+//
+// METAL Project
+//
+// Primary Author: Luke Jennings
+//
+
+var hdxOrderingAV = {
+    value: "ordering",
+    name: "Traversal Orderings",
+    decription: "Visualize different ways of sorting vertices in a 2D space.",
+
+    avActions: [
+        {
+            label: "START",
+            comment: "",
+            code: function(thisAV){
+
+            },
+            logMessage: function(thisAV){
+                return "Sorting waypoints based on the selected ordering";
+            }
+
+        }
+    ]
+}
